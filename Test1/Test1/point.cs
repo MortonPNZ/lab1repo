c1e82619678252dd8752dccf72deff2f424c40ce
MortonPNZ <morton.pnz@gmail.com>
2020-01-12 06:27:15 +0300

    lab4 ready

---
+++ Test1/Test1/point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Point
+    {
+        int x;
+        int y;
+        char sym;
+
+        public Point(int x, int y, char sym)
+        {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+        }
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
--- Test1/test1/Program.cs
+++ Test1/test1/Program.cs
@@ -8,9 +8,21 @@ namespace Test1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("test-repo");
+    
+            static void Main(string[] args)
+            {
+                HorizontalLine h1 = new HorizontalLine(0, 15, 0, '*');
+                h1.Draw();
+                HorizontalLine h2 = new HorizontalLine(0, 15, 10, '*');
+                h2.Draw();
+                VerticalLine v1 = new VerticalLine(0, 10, 0, '*');
+                v1.Draw();
+                VerticalLine v2 = new VerticalLine(0, 10, 15, '*');
+                v2.Draw();
+               
+
+                Console.ReadKey();
+            }
         }
     }
-}
+
